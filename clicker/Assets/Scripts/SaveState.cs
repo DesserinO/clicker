77024d1dd57440377ba3407f148045482f3f7205
DesserinO <forobzor@gmail.com>
2022-01-19 11:42:26 +0300

    Removing unnecessary files

--- clicker/Assets/Scripts/SaveState.cs
+++
@@ -1,9 +0,0 @@
-public class SaveState
-{
-    // NEED TO GET VALUES SOMEHOW
-    public float score = 123; 
-    /*public float scoreCoef;
-    public float idle;
-    public float idleCoef;
-    public ulong lvls;*/
-}
